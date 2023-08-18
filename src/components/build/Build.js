ebd60712dee7bc31701ddc511338e2453dba5815
Abhay Singh <abhaysinghparmar123@gmail.com>
2023-08-18 19:48:40 +0530

    first commit

---
+++ src/components/build/Build.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Build.module.css'
+
+const Build = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Build
---
+++ src/components/technology/Technology.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Technology.module.css'
+
+const Technology = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Technology
